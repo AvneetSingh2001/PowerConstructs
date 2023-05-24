edb0995e18edc6ae15ae9db5ce26ee8a0497f45a
Avneet Singh <avneetmaankiya@gmail.com>
2023-05-25 04:26:04 +0530

    Updated Clean Structure

--- app/src/main/java/com/avicodes/powerconstruct/presentation/MainActivity.kt
+++ app/src/main/java/com/avicodes/powerconstruct/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.avicodes.powerconstruct
+package com.avicodes.powerconstruct.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.avicodes.powerconstruct.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
